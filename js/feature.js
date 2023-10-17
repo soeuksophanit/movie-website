ac1e048c419204bdc64cba2d96b9e6620668815d
sophanit <sophanit@sophanits-MacBook-Pro.local>
2023-10-17 21:46:05 +0700

    change navabr link styles adding more style to QnA and tap-to-top

--- js/feature.js
+++ js/feature.js
@@ -1,12 +1,16 @@
 const navbar = document.querySelector(".my-navbar");
-let prevScrollPos = window.pageXOffset;
+const btnScroll = document.querySelector(".scroll-to-top");
+
+let prevScrollPos = window.pageYOffset;
 
 window.onscroll = function () {
   const currentScrollPos = window.pageYOffset;
   if (prevScrollPos > currentScrollPos) {
     navbar.style.top = "0";
+    btnScroll.classList.remove("active");
   } else {
     navbar.style.top = `-${navbar.offsetHeight}px`;
+    if (currentScrollPos > 200) btnScroll.classList.add("active");
   }
   prevScrollPos = currentScrollPos;
 };
@@ -21,5 +25,21 @@ for (let question of questions) {
       }
     }
     question.parentElement.classList.toggle("active");
+  });
+}
+
+// navbar click active
+const links = document.querySelectorAll(".link-item a");
+
+console.log(links);
+
+for (let eachLink of links) {
+  eachLink.addEventListener("click", () => {
+    for (let anotherLink of links) {
+      if (anotherLink !== eachLink) {
+        anotherLink.classList.remove("active");
+      }
+    }
+    eachLink.classList.toggle("active");
   });
 }
